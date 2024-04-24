8e079f7acb68b53ce930342c64ed75a5832c3611
ashtons event pack <159561871+ashtonsevents@users.noreply.github.com>
2024-04-24 09:35:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function downloadFile() {
     // Open the file in a new tab
     window.open(fileUrl, "_blank");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const expandButtons = document.querySelectorAll(".expand-button");
+
+    expandButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const post = this.parentElement;
+            const iframe = post.querySelector("iframe");
+            const buttonText = this.innerText;
+
+            if (buttonText === "See More") {
+                iframe.style.height = "auto";
+                this.innerText = "See Less";
+            } else {
+                iframe.style.height = "400px"; // Adjust the default height as needed
+                this.innerText = "See More";
+            }
+        });
+    });
+});

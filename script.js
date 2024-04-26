909327f984f7bf74473efffb399ec776610a2e1f
ashtons event pack <159561871+ashtonsevents@users.noreply.github.com>
2024-04-26 11:18:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     menuLinks.forEach(function(menuLink) {
         menuLink.addEventListener('click', function(event) {
             event.preventDefault();
+            // Remove 'selected' class from all menu items
+            menuLinks.forEach(function(link) {
+                link.classList.remove('selected');
+            });
+            // Add 'selected' class to the clicked menu item
+            this.classList.add('selected');
+            // Scroll to the target section
             var targetId = this.getAttribute('href');
             var targetElement = document.querySelector(targetId);
             if (targetElement) {
